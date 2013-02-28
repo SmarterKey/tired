b33105ed0686e3b9e67b653fd9b6770df4b19102
David SK app developer <david@smarterkey.com>
2013-02-28 21:37:46 +0100

    Initial Commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Administrator on 2/28/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
