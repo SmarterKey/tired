190a62f811640a3518858b216e7023cd17471d84
David SK app developer <david@smarterkey.com>
2013-02-28 21:56:38 +0100

    added MapForItinery class

---
+++ GitUseExample/MapForItinery.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinery.h
+//  GitUseExample
+//
+//  Created by Administrator on 2/28/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinery : NSObject
+
+@end
---
+++ GitUseExample/MapForItinery.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinery.m
+//  GitUseExample
+//
+//  Created by Administrator on 2/28/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapForItinery.h"
+
+@implementation MapForItinery
+
+@end
